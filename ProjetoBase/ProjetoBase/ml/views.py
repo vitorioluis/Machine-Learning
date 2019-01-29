50a5ba72b09555eebc538d774a475dc4a0065dea
vitorioluis@gmail.com <vitorioluis>
2019-01-29 10:47:31 -0200

    regressão logistica

--- ProjetoBase/ProjetoBase/ml/views.py
+++ ProjetoBase/ProjetoBase/ml/views.py
@@ -1,50 +1,59 @@
 # -*- encoding:utf-8
-import pandas as pd
 import numpy as np
+import pandas as pd
 from django.shortcuts import render
 from django.urls import reverse_lazy
 from django.views import View
 from django.views.generic import CreateView
 from sklearn.linear_model import LogisticRegression
 from sklearn.model_selection import train_test_split
-from sklearn.metrics import confusion_matrix,accuracy_score
 
 from .forms import IrisForm
 from .models import Iris
 
 
 def mod_regressao_logistica(lst):
+    """
+    Machine Learning na pratica
+    """
+    lst_colunas=['SepalLengthCm', 'SepalWidthCm', 'PetalLengthCm', 'PetalWidthCm', 'Species']
     # converter a list com os dados para predição em dataframe
-    lst_predict = pd.DataFrame(np.array(lst).reshape(1,4),
-                                 columns=['SepalLengthCm', 'SepalWidthCm', 'PetalLengthCm', 'PetalWidthCm'])
+    lst_predict = pd.DataFrame(np.array(lst).reshape(1, 4),
+                               columns=lst_colunas[:-1])
 
     # dados de iris obtido da base de dados
     data = Iris.objects.all()
     df = pd.DataFrame.from_records(
         data.values('SepalLengthCm', 'SepalWidthCm', 'PetalLengthCm', 'PetalWidthCm', 'Species')
     )
-    x = df[['SepalLengthCm', 'SepalWidthCm', 'PetalLengthCm', 'PetalWidthCm']]
+    x = df[lst_colunas[:-1]]
     y = df['Species']
 
     # treinando o modelo
-    x_train, x_teste, y_train, y_teste = train_test_split(x, y, test_size=100, random_state=101)
+    x_train, x_teste, y_train, y_teste = train_test_split(x, y, test_size=0.8, random_state=101)
     lm = LogisticRegression()
     lm.fit(x_train, y_train)
 
     # predicao
     predicao = lm.predict(lst_predict)
 
+    # y_teste=y_teste.values.reshape((len(y_teste),1))
+    # print(y_teste)
     # testes para validar o modelo
-    mtx = confusion_matrix(y_teste,predicao)
-    acc = round(accuracy_score(y_teste,predicao),2)
-    print(mtx,acc)
+    # mtx = confusion_matrix(y_teste,predicao)
+    # acc = accuracy_score(y_teste, predicao)
+    # print(mtx,acc)
+    #
+    # dic = {'predicao': predicao
+    #        # ,'mtx':mtx
+    #        # ,'acc':acc
+    #        }
 
-    dic = {'predicao': predicao,'mtx':mtx,'acc':acc}
-
-    return dic
+    return predicao[0]
 
 
 class LogisticIris(CreateView):
+    __title = '| Regressão Logistica'
     template_name = 'new_edit.html'
     models = Iris
     form_class = IrisForm
@@ -52,14 +61,18 @@ class LogisticIris(CreateView):
 
     def get_context_data(self, **kwargs):
         context = super(LogisticIris, self).get_context_data(**kwargs)
+        context['title'] = self.__title
         return context
 
     def post(self, request, *args, **kwargs):
+        context = {'form': self.form_class(),
+                   'title': self.__title}
         form = IrisForm(request.POST or None)
         if form.is_valid():
             lst = [float(str(n).replace(',', '.')) for n in form.cleaned_data.values()]
-            dic = mod_regressao_logistica(lst)
-            return render(request,self.template_name,dic)
+            context['predict'] = mod_regressao_logistica(lst)
+
+        return render(request, self.template_name, context)
 
 
 class ListaIris(View):
--- ProjetoBase/teste.py
+++ ProjetoBase/teste.py
@@ -5,7 +5,7 @@ __author__ = 'Luís Vtório'
 import pandas as pd
 
 from sklearn.datasets import load_iris
-from sklearn.linear_model import LinearRegression
+from sklearn.linear_model import LogisticRegression
 from sklearn.model_selection import train_test_split
 
 data = load_iris()
@@ -14,8 +14,15 @@ df = pd.DataFrame(data.data, columns=data.feature_names)
 x = df[['sepal length (cm)', 'sepal width (cm)', 'petal length (cm)']]
 y = df['petal width (cm)']
 
-x_train, x_teste, y_train, y_teste = train_test_split(x, y, test_size=0.5)
-lm = LinearRegression()
+x_train, x_teste, y_train, y_teste = train_test_split(x, y, test_size=0.5, random_state=101)
+lm = LogisticRegression()
 lm.fit(x_train, y_train)
 predicao = lm.predict(x_teste)
+
+# cr = classification_report(y_teste,predicao)
+
+# mtx = confusion_matrix(y_teste,predicao)
+# acc = accuracy_score(y_teste,predicao)
 print(predicao)
+
+np.n
