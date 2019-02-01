f9f74b800a9ddbcedc47515d772f966aadf023f5
vitorioluis@gmail.com <vitorioluis>
2019-02-01 17:38:54 -0200

    arknoid

--- Arknoid/ArkNoid_IA.py
+++ Arknoid/ArkNoid_IA.py
@@ -9,11 +9,15 @@ from pygame.locals import *
 class ArkNoid_ai:
     def __init__(self):
         pygame.init()
-        self.__img_nave = pygame.image.load('img/nave.png')
+        self.__img_nave = pygame.image.load('img/nav24.png')
         self.__img_base = pygame.image.load('img/base.png')
+        self.__img_fundo = pygame.image.load('img/fundo.jpg')
 
-        self.__resolucao_tela = 640, 480
+        self.__resolucao_tela = 600, 480
         self.__screen = pygame.display.set_mode(self.__resolucao_tela)
+
+        self.__screen.blit(self.__img_fundo, (0, 0))
+        pygame.display.flip()
 
         if pygame.font:
             self.font = pygame.font.Font(None, 25)
@@ -29,17 +33,16 @@ class ArkNoid_ai:
         self.__branco = (255, 255, 255)
         self.__vermelho = (255, 0, 0)
 
-        self.__nave_largura, self.__nave_altura = 0, 0
-        # self.__nave_y = self.__resolucao_tela[1]
-        self.__max_pa_x = self.__resolucao_tela[0] - self.__nave_largura
+        self.__max_pa_x = self.__resolucao_tela[0]
+        self.__pa_diametro = 90
 
-        self.__nave_diametro = 95
+        self.__nave_diametro = 70
         self.__nave_area = self.__nave_diametro / 2
         self.__max_nave_x = self.__resolucao_tela[0] - self.__nave_diametro
         self.__max_nave_y = self.__resolucao_tela[1] - self.__nave_diametro + 20
 
         self.__xpos = 310
-        self.__ypos = randint(100, self.__resolucao_tela[1] - self.__nave_altura)
+        self.__ypos = self.__resolucao_tela[1]
 
         self.__menos, self.__mais = 5, 5
 
@@ -47,28 +50,29 @@ class ArkNoid_ai:
         self.__time = pygame.time.Clock()
         self.__status = self.__status_nave_pa
         self.__lst_pos_nave = [5, -5]
-        # self.__base = pygame.Rect(283, self.__nave_y, self.__nave_largura, self.__nave_altura)
+
+        self.__base = pygame.Rect(300, self.__max_pa_x, self.__pa_diametro, self.__pa_diametro)
         self.__nave = pygame.Rect(300, self.__max_nave_y, self.__nave_diametro, self.__nave_diametro)
 
     def __movimentar_nave(self):
         self.__nave.left += self.__lst_pos_nave[0]
         self.__nave.top += self.__lst_pos_nave[1]
 
-        if self.__nave.left <= 0:
-            print(self.__nave.left, self.__nave.top)
+        if self.__nave.left <= 0:  # esquerda - 4
+            print('4', self.__nave.left, self.__nave.top)
             self.__nave.left = 0
             self.__lst_pos_nave[0] = -self.__lst_pos_nave[0]
-        elif self.__nave.left >= self.__max_nave_x:
-            print(self.__nave.left, self.__nave.top)
+        elif self.__nave.left >= self.__max_nave_x:  # direita - 2
+            print('2', self.__nave.left, self.__nave.top)
             self.__nave.left = self.__max_nave_x
             self.__lst_pos_nave[0] = -self.__lst_pos_nave[0]
 
-        if self.__nave.top < 0:
-            print(self.__nave.left, self.__nave.top)
+        if self.__nave.top < 0:  # top - 3
+            print('3', self.__nave.left, self.__nave.top)
             self.__nave.top = 0
             self.__lst_pos_nave[1] = -self.__lst_pos_nave[1]
-        elif self.__nave.top >= self.__max_nave_y:
-            print(self.__nave.left, self.__nave.top)
+        elif self.__nave.top >= self.__max_nave_y:  # bottom - 1
+            print('1', self.__nave.left, self.__nave.top)
             self.__nave.top = self.__max_nave_y
             self.__lst_pos_nave[1] = -self.__lst_pos_nave[1]
 
@@ -92,7 +96,7 @@ class ArkNoid_ai:
                     sys.exit()
 
             self.__time.tick(50)
-            self.__screen.fill(self.__branco)
+            # self.__screen.fill(self.__preto)
 
             self.__tecla = pygame.key.get_pressed()
 
@@ -104,17 +108,15 @@ class ArkNoid_ai:
                 self.__movimentar_pa()
                 self.__status = self.state_playing
 
-            self.__screen.fill(self.__branco)
+            # atualizando img de fundo
+            self.__screen.blit(self.__img_fundo, (0, 0))
 
+            # movendo a img
             self.__screen.blit(self.__img_nave, (self.__nave.left, self.__nave.top))
-            # self.__screen.blit(self.__base_img,(self.__nave.left + int(self.__nave), self.__nave.top + int(self.__raio_nave))                              )
-            #
-            # self.__nave = pygame.draw.rect(self.__screen, self.__preto, self.__nave)
-            # # self.__nave=pygame.draw.circle(self.__screen, self.__preto, (self.__nave.left + int(self.__raio_nave),
-            #                                                 self.__nave.top + int(self.__raio_nave)),
-            #                                                 int(self.__raio_nave))
+            self.__screen.blit(self.__img_base, (self.__base.left, self.__base.top))
 
-            pygame.display.update()
+            pygame.display.flip()
+            # pygame.display.update()
 
 
 if __name__ == "__main__":
