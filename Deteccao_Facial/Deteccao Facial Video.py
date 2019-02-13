360f2a49b6815e4309910c37c467dda2b4573293
vitorioluis@gmail.com <vitorioluis>
2019-02-13 14:32:55 -0200

    Detecção Facil Video

--- Deteccao_Facial/Deteccao Facial Video.py
+++ Deteccao_Facial/Deteccao Facial Video.py
@@ -1,11 +1,7 @@
 # -*- coding: utf-8 -*-
-
-from glob import glob
-
 import cv2
 
 # documentação do Opencv => https://opencv-python-tutroals.readthedocs.io
-
 
 # cores
 COR_RET_FACE = (0, 255, 0)
@@ -14,30 +10,30 @@ COR_RET_EYE = (255, 0, 0)
 # obter os cascades em => https://github.com/opencv/opencv/tree/master/data/haarcascades
 cascade_face_path = 'cascade/haarcascade_frontalface_default.xml'
 cascade_eye_open_path = 'cascade/haarcascade_eye_tree_eyeglasses.xml'
+
 # criar classificadores
 clf_face = cv2.CascadeClassifier(cascade_face_path)
 clf_eye_open = cv2.CascadeClassifier(cascade_eye_open_path)
 
+cap = cv2.VideoCapture(0)
 font = cv2.FONT_HERSHEY_SIMPLEX
 
-for image_path in glob('images/*jpg'):
-    # ler imagem
-    img = cv2.imread(image_path)
+while True:
+    ret, frame = cap.read()
+    if ret:
+        # converte em cinza
+        frame_gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+        faces = clf_face.detectMultiScale(frame_gray, scaleFactor=1.1, minNeighbors=5, minSize=(30, 30))
 
-    # converte em cinza
-    image_gray = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
-    faces = clf_face.detectMultiScale(image_gray, scaleFactor=1.1, minNeighbors=1, minSize=(1, 1))
-
-    # verifica se consegui detectar
-    if len(faces) > 0:
+        # verifica se consegui detectar
         for x, y, w, h in faces:
             # desenha um retangulo no rosto
-            img = cv2.rectangle(img, (x, y), (x + w, y + h), COR_RET_FACE, 2)
-            cv2.putText(img, 'Boiola', (x, y), font, 1, COR_RET_EYE, 2, cv2.LINE_AA)
+            frame = cv2.rectangle(frame, (x, y), (x + w, y + h), COR_RET_FACE, 2)
+            cv2.putText(frame, 'Boiola', (x - 10, y - 10), font, 1, COR_RET_EYE, 2, cv2.LINE_AA)
 
             # detecção de olhos
-            r_gray = image_gray[y:y + h, x:x + w]
-            r_color = img[y:y + h, x:x + w]
+            r_gray = frame_gray[y:y + h, x:x + w]
+            r_color = frame[y:y + h, x:x + w]
 
             # olhos abertos
             open_eyes = clf_eye_open.detectMultiScale(r_gray)
@@ -45,9 +41,11 @@ for image_path in glob('images/*jpg'):
                 # desenha o retangulo nos olhos
                 cv2.rectangle(r_color, (ex, ey), (ex + ew, ey + eh), COR_RET_EYE, 2)
 
+        cv2.imshow('Video', frame)
+        k = cv2.waitKey(30) & 0xff
 
+        if k == 27:
+            break
 
-        cv2.imshow('image', img)
-        cv2.waitKey(0)
-
-        cv2.destroyAllWindows()
+cap.release()
+cv2.destroyAllWindows()
