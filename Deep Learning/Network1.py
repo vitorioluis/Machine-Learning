c714de9c8139449ccc481083c327114e3eb839a5
vitorioluis@gmail.com <vitorioluis@gmail.com>
2019-04-03 22:53:47 -0300

    twitter

--- Deep Learning/Network1.py
+++ Deep Learning/Network1.py
@@ -15,6 +15,7 @@ próximos capítulos do livro.
 import random
 import numpy as np
 
+
 # Classe Network
 class Network(object):
 
@@ -37,7 +38,7 @@ class Network(object):
     def feedforward(self, a):
         """Retorna a saída da rede se `a` for input."""
         for b, w in zip(self.biases, self.weights):
-            a = sigmoid(np.dot(w, a)+b)
+            a = sigmoid(np.dot(w, a) + b)
         return a
 
     def SGD(self, training_data, epochs, mini_batch_size, eta, test_data=None):
@@ -59,13 +60,13 @@ class Network(object):
 
         for j in range(epochs):
             random.shuffle(training_data)
-            mini_batches = [training_data[k:k+mini_batch_size] for k in range(0, n, mini_batch_size)]
-            
+            mini_batches = [training_data[k:k + mini_batch_size] for k in range(0, n, mini_batch_size)]
+
             for mini_batch in mini_batches:
                 self.update_mini_batch(mini_batch, eta)
-            
+
             if test_data:
-                print("Epoch {} : {} / {}".format(j,self.evaluate(test_data),n_test));
+                print("Epoch {} : {} / {}".format(j, self.evaluate(test_data), n_test));
             else:
                 print("Epoch {} finalizada".format(j))
 
@@ -76,14 +77,14 @@ class Network(object):
 
         nabla_b = [np.zeros(b.shape) for b in self.biases]
         nabla_w = [np.zeros(w.shape) for w in self.weights]
-        
+
         for x, y in mini_batch:
             delta_nabla_b, delta_nabla_w = self.backprop(x, y)
-            nabla_b = [nb+dnb for nb, dnb in zip(nabla_b, delta_nabla_b)]
-            nabla_w = [nw+dnw for nw, dnw in zip(nabla_w, delta_nabla_w)]
-        
-        self.weights = [w-(eta/len(mini_batch))*nw for w, nw in zip(self.weights, nabla_w)]
-        self.biases = [b-(eta/len(mini_batch))*nb for b, nb in zip(self.biases, nabla_b)]
+            nabla_b = [nb + dnb for nb, dnb in zip(nabla_b, delta_nabla_b)]
+            nabla_w = [nw + dnw for nw, dnw in zip(nabla_w, delta_nabla_w)]
+
+        self.weights = [w - (eta / len(mini_batch)) * nw for w, nw in zip(self.weights, nabla_w)]
+        self.biases = [b - (eta / len(mini_batch)) * nb for b, nb in zip(self.biases, nabla_b)]
 
     def backprop(self, x, y):
         """Retorna uma tupla `(nabla_b, nabla_w)` representando o
@@ -93,35 +94,35 @@ class Network(object):
 
         nabla_b = [np.zeros(b.shape) for b in self.biases]
         nabla_w = [np.zeros(w.shape) for w in self.weights]
-        
+
         # Feedforward
         activation = x
 
         # Lista para armazenar todas as ativações, camada por camada
-        activations = [x] 
+        activations = [x]
 
         # Lista para armazenar todos os vetores z, camada por camada
-        zs = [] 
+        zs = []
 
         for b, w in zip(self.biases, self.weights):
-            z = np.dot(w, activation)+b
+            z = np.dot(w, activation) + b
             zs.append(z)
             activation = sigmoid(z)
             activations.append(activation)
-        
+
         # Backward pass
         delta = self.cost_derivative(activations[-1], y) * sigmoid_prime(zs[-1])
         nabla_b[-1] = delta
         nabla_w[-1] = np.dot(delta, activations[-2].transpose())
-        
+
         # Aqui, l = 1 significa a última camada de neurônios, l = 2 é a segunda e assim por diante.
         for l in range(2, self.num_layers):
             z = zs[-l]
             sp = sigmoid_prime(z)
-            delta = np.dot(self.weights[-l+1].transpose(), delta) * sp
+            delta = np.dot(self.weights[-l + 1].transpose(), delta) * sp
             nabla_b[-l] = delta
-            nabla_w[-l] = np.dot(delta, activations[-l-1].transpose())
-        return (nabla_b, nabla_w)
+            nabla_w[-l] = np.dot(delta, activations[-l - 1].transpose())
+        return nabla_b, nabla_w
 
     def evaluate(self, test_data):
         """Retorna o número de entradas de teste para as quais a rede neural 
@@ -134,12 +135,14 @@ class Network(object):
 
     def cost_derivative(self, output_activations, y):
         """Retorna o vetor das derivadas parciais."""
-        return (output_activations-y)
+        return output_activations - y
+
 
 # Função de Ativação Sigmóide
 def sigmoid(z):
-    return 1.0/(1.0+np.exp(-z))
+    return 1.0 / (1.0 + np.exp(-z))
+
 
 # Função para retornar as derivadas da função Sigmóide
 def sigmoid_prime(z):
-    return sigmoid(z)*(1-sigmoid(z))
+    return sigmoid(z) * (1 - sigmoid(z))
