1857130d862696c8af4357f3fdab09907efbb78d
vitorioluis@gmail.com <vitorioluis@gmail.com>
2019-04-07 21:17:10 -0300

    heoku

--- LabsML/LabsML/core/urls.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf8 -*-
-
-from django.conf.urls import url
-
-from . import views
-
-urlpatterns = [
-    url(r'^$', views.home, name='home'),
-]
--- LabsML/LabsML/core/views.py
+++
@@ -1,7 +0,0 @@
-# -*-coding:utf-8-*-
-
-from django.shortcuts import render
-
-
-def home(request):
-    return render(request, 'index.html')
